550c1786fc6eddb319b4dd52e771626cfb31d80c
jjc144 <155926239+jjc144@users.noreply.github.com>
2024-01-23 19:58:36 -0500

    First verison
    OOP 2024 example

---
+++ PetsOOP/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+
+using ClassLibrary1;
+
+string input = "";
+
+while (input != "4")
+{ 
+Console.WriteLine("Welcome to the Pet Store");
+Console.WriteLine("1. Add a new Pet");
+Console.WriteLine("2. List all pets");
+Console.WriteLine("3. Have 2 pets meet");
+Console.WriteLine("4. Exit");
+    input = Console.ReadLine();
+    switch (input)
+    {
+        case "1":
+            Console.WriteLine("1. Add a new cat");
+            string petname = Console.ReadLine();
+            Console.WriteLine("2. Add a new Chimpanzee");
+            Console.WriteLine("3. add a new Turtle");
+            string input2 = Console.ReadLine();
+            switch (input2)
+            {
+                case "1":
+                    Cat newCar = new Cat(petname);
+                    break;
+                case "2":
+                    Chimpanzee newChimp = new Chimpanzee(petname);
+                    break;
+                case "3":
+                    Turtle newTurtle = new Turtle(petname);
+                    break;
+
+            }
+            break;
+        case "2":
+            break;
+        case "3":
+            break;
+        case "4":
+            break;
+        default:
+            break;
+
+        
+                 
+    }
+
+}
